c18000a1cd2ddb9ef0cb0c964125143ac3a501de
M4ma1 <togumbaevr@mail.ru>
2023-12-04 20:17:20 +0700

    patch 1

--- haffman.py
+++ haffman.py
@@ -19,21 +19,18 @@ class Node:
     def getLetterCount(message):
         # Returns a dictionary with keys of single letters and values of the
         # count of how many times they appear in the message parameter.
-        letterCount = {
-            'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0,
-            'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0,
-            'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0,
-            'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0,
-            'А': 0, 'Б': 0, 'В': 0, 'Г': 0, 'Д': 0, 'Е': 0, 'Ё': 0, 'Ж': 0, 'З': 0, 'И': 0, 'Й': 0, 'К': 0, 'Л': 0,
-            'М': 0, 'Н': 0, 'О': 0, 'П': 0, 'Р': 0, 'С': 0, 'Т': 0, 'У': 0, 'Ф': 0, 'Х': 0, 'Ц': 0, 'Ч': 0, 'Ш': 0,
-            'Щ': 0, 'Ъ': 0, 'Ы': 0, 'Ь': 0, 'Э': 0, 'Ю': 0, 'Я': 0, 'а': 0, 'б': 0, 'в': 0, 'г': 0, 'д': 0, 'е': 0,
-            'ё': 0, 'ж': 0, 'з': 0, 'и': 0, 'й': 0, 'к': 0, 'л': 0, 'м': 0, 'н': 0, 'о': 0, 'п': 0, 'р': 0, 'с': 0,
-            'т': 0, 'у': 0, 'ф': 0, 'х': 0, 'ц': 0, 'ч': 0, 'ш': 0, 'щ': 0, 'ъ': 0, 'ы': 0, 'ь': 0, 'э': 0, 'ю': 0,
-            'я': 0, '!':0, ' ':0, ',':0, '.':0, '-':0, ':':0, '?':0, ';':0, '\"':0, '\n':0, '\'':0
-        }
+        letterCount = {'?':0,'Å':0}
+
         for letter in message:
-            if letter in letterCount:
+            if letter not in letterCount:
+                letterCount[letter] = 0
+            
+            else:
                 letterCount[letter] += 1
+            # if letter in letterCount:
+                # letterCount[letter] += 1
+            if letter =='?':
+                continue
 
         non_empty_list = dict([(k,v) for k,v in letterCount.items() if v])
         sorted_non_empty_list = dict(sorted(non_empty_list.items(), key=lambda x:x[1]))
@@ -101,10 +98,10 @@ def decode_message(encoded_message, huffman_tree):
     return decoded_message
 
 txt = open('file.txt', 'r').read()
-
 huffman_tree_root = Node.build_huffman_tree(Node.getLetterCount(txt))
 huffman_tree_root.print_tree()
 huffman_codes_mapping = Node.huffman_codes(huffman_tree_root)
+
 
 enc_aboba = encode_message(txt, huffman_codes_mapping)
 print(enc_aboba)

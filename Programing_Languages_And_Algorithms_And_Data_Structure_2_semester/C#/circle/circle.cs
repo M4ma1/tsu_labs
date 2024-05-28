10a27b0faac25e571f301149480824560b9f79cc
rus1k <togumbaevr@mail.ru>
2024-05-28 11:50:32 +0700

    report and workful lab

--- Programing_Languages_And_Algorithms_And_Data_Structure_2_semester/C#/circle/circle.cs
+++ Programing_Languages_And_Algorithms_And_Data_Structure_2_semester/C#/circle/circle.cs
@@ -1,77 +1,204 @@
 using System;
 
-namespace Rusik{
-    public class Circle{
+namespace Rusik {
+    class TestRunner {
+      
+        public static void RunTests()
+        {
+            TestCircleInput();
+            TestCircleOutput();
+            TestCircleSquare();
+            TestCircleCircumference();
+            TestCircleMul();
+            TestCircleEquation();
+            TestCircleContainsPoint();
+            TestCircleIntersection();
+            TestCircleContains();
+            TestCircleRadius();
+        }
+
+        private static void AssertEqual(double expected, double actual, double tolerance = 0.0001)
+        {
+            if (Math.Abs(expected - actual) > tolerance)
+            {
+                throw new Exception($"Assertion failed. Expected: {expected}, Actual: {actual}");
+            }
+        }
+
+        private static void AssertTrue(bool condition)
+        {
+            if (!condition)
+            {
+                throw new Exception("Assertion failed. Condition is not true.");
+            }
+        }
+
+        private static void AssertFalse(bool condition)
+        {
+            if (condition)
+            {
+                throw new Exception("Assertion failed. Condition is not false.");
+            }
+        }
+
+        private static void TestCircleInput()
+        {
+            var circle = new Circle(0, 0, 5);
+            AssertEqual(5, circle.r);
+
+            try
+            {
+                new Circle(0, 0, -5);
+                throw new Exception("Expected exception for negative radius not thrown.");
+            }
+            catch (Exception) { }
+        }
+
+        private static void TestCircleOutput()
+        {
+            var circle = new Circle(0, 0, 5);
+            AssertEqual(5, circle.r);
+        }
+
+        private static void TestCircleSquare()
+        {
+            var circle = new Circle(0, 0, 5);
+            AssertEqual(Math.PI * 25, circle.square());
+        }
+
+        private static void TestCircleCircumference()
+        {
+            var circle = new Circle(0, 0, 5);
+            AssertEqual(2 * Math.PI * 5, Circle.circumference(circle));
+        }
+
+        private static void TestCircleMul()
+        {
+            var circle = new Circle(0, 0, 5);
+            var newCircle = Circle.mul(circle, 2);
+            AssertEqual(10, newCircle.r);
+        }
+
+        private static void TestCircleEquation()
+        {
+            var circle = new Circle(0, 0, 5);
+
+            var circle2 = new Circle(1, 2, 3);
+            AssertFalse(Circle.equation(circle, circle2));
+        }
+
+        private static void TestCircleContainsPoint()
+        {
+            var circle = new Circle(0, 0, 5);
+            AssertTrue(Circle.containsPoint(3, 4, circle));
+            AssertTrue(Circle.containsPoint(0, 0, circle));
+            AssertFalse(Circle.containsPoint(6, 0, circle));
+        }
+
+        private static void TestCircleIntersection()
+        {
+            var circle1 = new Circle(0, 0, 5);
+            var circle2 = new Circle(8, 0, 5);
+            var circle3 = new Circle(11, 0, 5);
+
+            AssertTrue(Circle.intersection(circle1, circle2));
+            AssertFalse(Circle.intersection(circle1, circle3));
+        }
+
+        private static void TestCircleContains()
+        {
+            var circle1 = new Circle(0, 0, 10);
+            var circle2 = new Circle(0, 0, 5);
+            var circle3 = new Circle(8, 0, 5);
+
+            AssertTrue(Circle.contains(circle1, circle2));
+            AssertFalse(Circle.contains(circle1, circle3));
+        }
+
+        private static void TestCircleRadius()
+        {
+            var circle = new Circle(0, 0, 5);
+            AssertEqual(5, circle.r);
+
+            var newCircle = Circle.mul(circle, 2);
+            AssertEqual(10, newCircle.r);
+        }
+}
+
+    interface Figure{
+        double square();
+        void input();
+        void output();
+    }
+    public class Circle : Figure{
         public static void Main(string[] args){
-            Circle asd = new Circle();
-            asd.input();
-            Circle qwe = new Circle();
-            qwe.input();
-            asd.output();
-            qwe.output();
-
-            Console.WriteLine(asd.square());
-            Console.WriteLine(asd.circumference());
-            asd = asd.mul(3);
-            asd.output();
-
-            Console.WriteLine(qwe.equation(asd));
-            Console.WriteLine(qwe.containsPoint(0, 0));
-            Console.WriteLine(qwe.contains(asd));
-            Console.WriteLine(asd.intersection(qwe));
-
-            Console.WriteLine(asd.radius(qwe));
+            TestRunner.RunTests();
         }
-        int x, y, r;
-        Circle(int center_x=1, int center_y=1, int radius=1){
-            x = center_x;
-            y = center_y;
-            r = radius;
+        double _x, _y, _r;
+        public Circle(double center_x=1, double center_y=1, double radius=1){
+            this._x = center_x;
+            this._y = center_y;
+            if(radius <= 0){throw new Exception("Negative or null radius");}
+            this._r = radius;
         }
-        void input(){
+        public double x{
+            get{return _x;}
+            set{_x = value;}
+        }
+        public double y{
+            get{return _y;}
+            set{_y = value;}
+        }
+        public double r{
+            get{return _r;}
+            set{
+                if (value <= 0){throw new Exception("Negative or null radius");} 
+                _r = value;}
+        }
+        void Figure.input(){
             Console.WriteLine("input x coordinate: ");
-            x = Convert.ToInt32(Console.ReadLine());
+            x = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("input y coordinate: ");
-            y = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("input radius: ");
-            r = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            r = Math.Abs(Convert.ToDouble(Console.ReadLine()));
         }
-        void output(){
+        void Figure.output(){
             Console.WriteLine("x coordinate: {0}", x);
             Console.WriteLine("y coordinate: {0}", y);
             Console.WriteLine("r coordinate: {0}", r);
         }
-        int square(){
-            return Convert.ToInt32(Math.PI * r * r);
+        public double square(){
+            return Math.PI * r * r;
         }
-        int circumference(){
-            return Convert.ToInt32(2 * Math.PI * r);
+        public static double circumference(Circle circle){
+            return 2 * Math.PI * circle.r;
         }
-        Circle mul(int n){
-            r *= n;
-            return this;
+        public static Circle mul(Circle circle, int n){
+            circle.r *= n;
+            return circle;
         }
-        bool equation(Circle circle2){
-            return r == circle2.r;
+        public static bool equation(Circle circle, Circle circle2){
+            return circle.r == circle2.r;
         }
-        public bool containsPoint(int xPoint, int yPoint){
-            double distance = Math.Sqrt(Math.Pow(xPoint - x, 2) + Math.Pow(yPoint - y, 2));
-            return distance <= r;
+        public static bool containsPoint(double xPoint, double yPoint, Circle circle){
+            double distance = Math.Sqrt(Math.Pow(xPoint - circle.x, 2) + Math.Pow(yPoint - circle.y, 2));
+            return distance <= circle.r;
         }
-        bool intersection(Circle circle1){
-            int tmp = (x+circle1.x)*(x+circle1.x) + (y+circle1.y)*(y+circle1.y);
-            return ((r-circle1.r)*(r-circle1.r) <= tmp && tmp <= (r+circle1.r)*(r+circle1.r));
-        }
-
-        bool contains(Circle circle1){
-            int d = Convert.ToInt32(Math.Sqrt((x - circle1.x)*(x - circle1.x) + (y - circle1.y)*(y - circle1.y)));
-            return r > d + circle1.r;
+        public static bool intersection(Circle circle1, Circle circle2){
+            double tmp = (circle2.x+circle1.x)*(circle2.x+circle1.x) + (circle2.y+circle1.y)*(circle2.y+circle1.y);
+            return ((circle1.r-circle2.r)*(circle1.r-circle2.r) <= tmp && tmp <= (circle2.r+circle1.r)*(circle2.r+circle1.r));
         }
 
-        float radius(Circle circle1){
-            if(intersection(circle1)){
-                int d = Convert.ToInt32(Math.Sqrt((x - circle1.x)*(x - circle1.x) + (y - circle1.y)*(y - circle1.y)));
-                return (float)Convert.ToDouble((r + circle1.r - d))/2;
+        public static bool contains(Circle circle1, Circle circle2){
+            double d = Math.Sqrt((circle1.x - circle2.x)*(circle1.x - circle2.x) + (circle1.y - circle2.y)*(circle1.y - circle2.y));
+            return circle1.r > d + circle2.r;
+        }
+
+        public static float radius(Circle circle1, Circle circle2){
+            if(intersection(circle1, circle2)){
+                double d = Math.Sqrt((circle1.x - circle2.x)*(circle1.x - circle2.x) + (circle1.y - circle2.y)*(circle1.y - circle2.y));
+                return (float)Convert.ToDouble((circle1.r + circle2.r - d))/2;
             }
             return 0;
         }
